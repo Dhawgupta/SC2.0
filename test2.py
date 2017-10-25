0c09d52e5536de3138fc3c3401901143c21e95a1
Dhawgupta <dhawal.cs15p@iitp.ac.in>
2017-10-25 15:25:43 +0530

    Modified test2.py with median

--- test2.py
+++ test2.py
@@ -1,6 +1,19 @@
 from hx711 import Hx711
 a = Hx711(48,14)
+abuf = []
 while 1:
-	print a.read()
+	abuf = []
+	for i in range(10):
+		abuf.append(a.read())
+	
+	abuf.sort()
+	string = str()
+	for i in range(10):
+		string = string  +  str(abuf[i]) + " "
+	
+	aval = (abuf[4] + abuf[3] + abuf[5] + abuf[6])/4	
+	print aval
+	print string
+
 
 
